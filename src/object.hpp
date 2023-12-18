701f07c514a26fe39fdd3e246564bba5fbad1686
Steve Theis <steve.theis@wolflair.com>
2023-12-18 13:52:44 -0600

    WIP on InternedStringKey

--- src/object.hpp
+++ src/object.hpp
@@ -1,6 +1,7 @@
 #ifndef ppclox_object_hpp
 #define ppclox_object_hpp
 
+#include <functional>
 #include <unordered_map>
 #include <string>
 #include <string_view>
@@ -26,6 +27,54 @@ private:
 
 //TODO: Implement custom new and delete for objs? And how do we account for the ObjStrings
 // memory?   
+};
+
+class InternedStringKey {
+public:
+//FIX - Move this to CPP file.
+    bool operator==(const InternedStringKey& key) const {
+        // Thanks to de-duping as well as the way these keys are used, 
+        // ObjString* pointers indicate equivalent objects
+        if (this->m_obj_string != nullptr && key.m_obj_string != nullptr) {
+            return this->m_obj_string == key.m_obj_string;
+        }
+
+        // If the hashes differ, we know we are different
+        if (this->m_hash != key.m_hash) {
+            return false;
+        }
+
+        // If hashes are the same, we must compare strings directly
+        return this->m_string_view == key.m_string_view;
+    }
+
+    // TODO: Need to add constructors that make sense for this type.
+    // Constructors must compute a hash, unless its been cached somewhere (e.g.
+    // on the ObjString).
+
+    const std::string_view string_view() const { return m_string_view; }
+    const ObjString* obj_string() const  { return m_obj_string; }
+    std::size_t hash() const { return m_hash; }
+private:
+    /** 
+     * When actually stored in the map, this will point to the actual 
+     * string data owned by the corresponding m_obj_string.
+    */
+    std::string_view m_string_view{};
+    /** This will be non-null when actually stored in the map. */
+    /** It can be null or non-null when searching within the map for a given key. */
+    ObjString* m_obj_string{};
+    std::size_t m_hash{};    
+};
+
+// Function object that knows how to hash InternedStringKey
+// See method 3 at https://marknelson.us/posts/2011/09/03/hash-functions-for-c-unordered-containers.html
+class InternedStringKeyHash {
+    size_t operator()(const InternedStringKey& key) const
+    {
+        //return std::hash<std::string_view>()(key.m_string_view);
+        return key.hash();
+    }
 };
 
 class ObjString : public Obj {
