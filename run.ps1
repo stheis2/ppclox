8d4ea67d60520e8f54aa6ce1356f89f1002d5b63
Steve Theis <steve.theis@wolflair.com>
2023-12-30 19:45:42 -0600

    Adjust run script so I can still use it when not debugging

--- run.ps1
+++ run.ps1
@@ -9,7 +9,7 @@ try {
         # NOTE! /EHsc is included to silence warning C4530: 
         #       C++ exception handler used, but unwind semantics are not enabled. 
         #       Specify /EHsc
-        cl /std:c++latest /EHsc ../src/*.cpp /link /out:pplox.exe
+        cl /std:c++latest /EHsc ../*.cpp /link /out:pplox.exe
         if ($LASTEXITCODE -ne 0) {
             Write-Host ""
             Write-Host "Non-zero exit code from cl: $LASTEXITCODE"
@@ -22,7 +22,7 @@ try {
     }
 
     Write-Host ""
-    ./build/pplox ./src/test_file.lox
+    ./build/pplox ./test_file.lox
 }
 finally {
     Pop-Location
